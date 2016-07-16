b80299b34e3fa19f315e1cc9b6ffe28af95189b8
alomina007 <alomina007@users.noreply.github.com>
2016-07-16 07:15:10 +0430

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from telebot import types
 import requests
 import simplejson as json
 
-TOKEN = 'YOUR_BOT_TOKEN'
+TOKEN = '260214096:AAFYr32mjThh6uxW0J89MqW68_hZXzTlJ3w'
 API_PATH = 'http://api.vajehyab.com/v2/public'
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(260214096:AAFYr32mjThh6uxW0J89MqW68_hZXzTlJ3w)
 
 
 @bot.message_handler(commands=['start'])
@@ -27,7 +27,7 @@ def help_command_handler(msg):
 استفاده از این ربات بسیار ساده است.
 کافی است کلمه مورد نظر خود را در پیام خصوصی برای ربات ارسال نمایید تا معنای آن را دریافت کنید.
 همچنین، برای استفاده اینلاین از ربات، کافی است پس از ارسال دستور /start در پیام شخصی ربات، برای گیرنده مورد نظر خود پیام زیر را نوشته و معنای آن را انتخاب نمایید تا ارسال شود.
-@IranBot کلمه
+@pcmotargem کلمه
 '''
     if msg.chat.type == 'private':
         bot.send_message(msg.chat.id, help_msg)
